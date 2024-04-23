e1e7805169f91e7c98a624f25ede258d8c300e33
hyrm <1358188945@qq.com>
2024-04-24 00:10:05 +0800

    同步地图放大逻辑

--- assets/main.ts
+++ assets/main.ts
@@ -19,6 +19,7 @@ export default class Main extends cc.Component {
     private mapQuadTree: QuadTree<tileMapData> = null
     private mapQuadSize: number = 16
 
+    private mapScale: number = 1
     private mapOriSize: { width: number, height: number } = { width: 1024 * 7, height: 1024 * 4 }
     private mapTileSize: { width: number, height: number } = { width: 1024 * 7 / 16, height: 1024 * 4 / 16 }
 
@@ -44,7 +45,6 @@ export default class Main extends cc.Component {
 
 
     private initTouchEventListener() {
-
         this.viewPort.on(cc.Node.EventType.TOUCH_MOVE, (event: cc.Event.EventTouch) => {
             if (!this.isTouching) return
 
@@ -70,6 +70,8 @@ export default class Main extends cc.Component {
 
         this.viewPort.on(cc.Node.EventType.TOUCH_START, (event: cc.Event.EventTouch) => {
             this.isTouching = true
+            const mapPos = this.mapContainer.convertToNodeSpaceAR(event.getLocation())
+            this.node.getChildByName("fixed").getChildByName("pos").getComponent(cc.Label).string = `(${mapPos.x.toFixed(2)}, ${mapPos.y.toFixed(2)})`
         }, this)
 
         this.viewPort.on(cc.Node.EventType.TOUCH_END, (event: cc.Event.EventTouch) => {
@@ -80,11 +82,24 @@ export default class Main extends cc.Component {
             this.isTouching = false
         }, this)
 
+        this.viewPort.on(cc.Node.EventType.MOUSE_WHEEL, (event: cc.Event.EventMouse) => {
+            const scrollY = event.getScrollY()
+            this.mapScale = scrollY > 0 ? Number(((this.mapScale + 0.1).toFixed(1))) : Number(((this.mapScale - 0.1).toFixed(1)))
+            this.mapContainer.scale = this.mapScale
+            this.rolesContainer.scale = this.mapScale
+            this.initTileMapQuadTree()
+            this.updateViewPortMapTileNodes()
+        })
     }
 
     private initTileMapQuadTree() {
 
-        const rootBounds: QuadTreeRect = { x: -this.mapOriSize.width / 2, y: -this.mapOriSize.height / 2, width: this.mapOriSize.width, height: this.mapOriSize.height }
+        const boundsPosX = -this.mapOriSize.width / 2
+        const boundsPosY = -this.mapOriSize.height / 2
+        const boundsWidth = this.mapOriSize.width * this.mapScale
+        const boundsHeight = this.mapOriSize.height * this.mapScale
+
+        const rootBounds: QuadTreeRect = { x: boundsPosX, y: boundsPosY, width: boundsWidth, height: boundsHeight }
         this.mapQuadTree = new QuadTree<tileMapData>(rootBounds, 4)
 
         for (let i = 0; i < Math.pow(this.mapQuadSize, 2); i++) {
@@ -115,7 +130,7 @@ export default class Main extends cc.Component {
         this.trianglesGraph = new GraphMatrix()
 
         // 多边形顶点(扁平、二维)
-        const polygonFlatVertices = [10, 0, 0, 50, 60, 60, 70, 10]
+        const polygonFlatVertices = [3415, 2150, 3390, 2204, 3415, 2264, 3390, 2322, 3382, 2375, 3430, 2431, 3483, 2502, 3520, 2536, 3578, 2568, 3688, 2612, 3752, 2636, 3736, 2671, 3671, 2652, 3641, 2625, 3576, 2604, 3526, 2578, 3509, 2552, 3454, 2515, 3414, 2476, 3393, 2444, 3357, 2380, 3346, 2346, 3369, 2299, 3372, 2266, 3341, 2296, 3306, 2301, 3282, 2301, 3275, 2301, 3266, 2266, 3270, 2196, 3316, 2191, 3343, 2172, 3362, 2104]
         const polygonVertices = []
         for (let i = 0; i < polygonFlatVertices.length; i += 2) {
             polygonVertices.push([polygonFlatVertices[i], polygonFlatVertices[i + 1]])
@@ -128,7 +143,7 @@ export default class Main extends cc.Component {
         const vertexMap = new Map<string, number>()
         for (let i = 0; i < triangleVerticesIndex.length; i += 3) {
 
-            // 构建三角形类
+            // 构建三角形类:给图中添加顶点
             const triangleVertices = triangleVerticesIndex.slice(i, i + 3)
             const vertice1 = polygonVertices[triangleVertices[0]]
             const vertice2 = polygonVertices[triangleVertices[1]]
@@ -136,10 +151,13 @@ export default class Main extends cc.Component {
             const triangle: Triangle = { id: Math.ceil(i / 3), vertices: [vertice1, vertice2, vertice3] }
             this.navMeshTriangles.push(triangle)
             this.trianglesGraph.addVertex(triangle.id)
+            this.drawTriangle(triangle)
 
+            // 构建三角形类:给图中添加顶点
             for (const vertice of triangle.vertices) {
 
                 const key = `${vertice[0]},${vertice[1]}`
+
                 if (!vertexMap.has(key)) {
 
                     vertexMap.set(key, triangle.id)
@@ -147,10 +165,8 @@ export default class Main extends cc.Component {
                 } else {
 
                     // 通过判断俩三角形是否存在邻接边,来构建图中邻接边
-
                     const neighborTriangleId = vertexMap.get(key)
                     const neighborTriangle = this.navMeshTriangles[neighborTriangleId]
-
                     const vertices1 = triangle.vertices.filter(v => v[0] !== vertice[0] || v[1] !== vertice[1])
                     const vertices2 = neighborTriangle.vertices.filter(v => v[0] !== vertice[0] || v[1] !== vertice[1])
 
@@ -163,24 +179,21 @@ export default class Main extends cc.Component {
                 }
 
             }
-
-
-
-
         }
-
-        console.log(this.trianglesGraph.print())
     }
 
     private updateViewPortMapTileNodes() {
 
-        // 计算 viewport 中心坐标点
-        const centerPos = new cc.Vec2(0 - (this.mapContainer.x + this.mapOriSize.width / 2), 0 - (this.mapContainer.y + this.mapOriSize.height / 2))
+        // 计算视口矩形
+        const viewportMidX = this.mapQuadTree.bounds.x + this.mapQuadTree.bounds.width / 2 / this.mapScale
+        const viewportMidY = this.mapQuadTree.bounds.y + this.mapQuadTree.bounds.height / 2 / this.mapScale
+
+        const viewportBoundX = (viewportMidX - this.viewPort.width / 2) - (this.mapContainer.x + 3584)
+        const viewportBoundY = (viewportMidY - this.viewPort.height / 2) - (this.mapContainer.y + 2048)
+        const viewportRect: QuadTreeRect = { x: viewportBoundX, y: viewportBoundY, width: this.viewPort.width, height: this.viewPort.height }
 
         // 判断视口与地图四叉树碰撞
-        const viewportRect: QuadTreeRect = { x: centerPos.x - this.viewPort.width / 2, y: centerPos.y - this.viewPort.height / 2, width: this.viewPort.width, height: this.viewPort.height }
         const visiableTileObjects: tileMapData[] = this.mapQuadTree.retrieve(viewportRect)
-
         for (const tileObject of visiableTileObjects) {
             if (!tileObject.node.getComponent(cc.Sprite)) {
                 tileObject.node.addComponent(cc.Sprite)
@@ -189,6 +202,31 @@ export default class Main extends cc.Component {
                 })
             }
         }
+    }
+
+
+    //临时代码
+    private drawLine(start: cc.Vec2, end: cc.Vec2) {
+        let ctx = this.rolesContainer.getComponent(cc.Graphics)
+        ctx.moveTo(start.x, start.y)
+        ctx.lineTo(end.x, end.y)
+        ctx.lineWidth = 5
+        ctx.strokeColor = cc.Color.RED
+        ctx.stroke()
+    }
+
+    private drawTriangle(triangle: Triangle) {
+        let ctx = this.rolesContainer.getComponent(cc.Graphics)
+        const vertice1 = triangle.vertices[0]
+        const vertice2 = triangle.vertices[1]
+        const vertice3 = triangle.vertices[2]
+        ctx.moveTo(vertice1[0], vertice1[1])
+        ctx.lineTo(vertice2[0], vertice2[1])
+        ctx.lineTo(vertice3[0], vertice3[1])
+        ctx.lineTo(vertice1[0], vertice1[1])
+        ctx.lineWidth = 5
+        ctx.strokeColor = cc.Color.RED
+        ctx.stroke()
     }
 }
 
